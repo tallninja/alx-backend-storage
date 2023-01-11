ebfb89f7293fd8ab5045b5fc0999ebaabbb48bb8
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-11 18:22:59 +0300

    3-glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- SQL
-SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2023) - formed) AS lifespan
 FROM metal_bands
 WHERE style='Glam rock'
 ORDER BY formed DESC;
