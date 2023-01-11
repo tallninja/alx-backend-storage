6d20d1fe45a1d51dad921f6aedb90c6d3343b263
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-11 16:30:44 +0300

    5-valid_email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+delimiter //
+CREATE trigger reset_valid 
+before update ON users
+for each row 
+BEGIN 
+    if NEW.email <> OLD.email 
+    THEN SET NEW.valid_email = 0;
+    end if;
+END // 
+delimiter ;
