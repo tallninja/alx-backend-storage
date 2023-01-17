5cf9651e4d8c88679a0461c7bcce7704076aed78
Ernest Wambua <ernestwambua2@gmail.com>
2023-01-17 10:59:02 +0300

    12-log_stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""12-log_stats.py"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    number_logs = collection.count_documents({})
+    print("""{} logs
+Methods:""".format(number_logs))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        logs = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, logs))
+
+    pathlogs = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(pathlogs))
